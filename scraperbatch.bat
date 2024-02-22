57b183392c6756c9102f0d8c6452ca5c7a973987
Mitchell Polser <111939878+MitchellPolser@users.noreply.github.com>
2024-02-22 13:42:57 -0500

    Add files via upload

---
+++ scraperbatch.bat
@@ -0,0 +1,4 @@
+@echo on
+call C:\Users\Mitchell\Desktop\DotaScraper\drive-download-20230608T152213Z-001\new_venv\Scripts\activate.bat
+cd C:\Users\Mitchell\Desktop\DotaScraper\drive-download-20230608T152213Z-001
+call C:\Users\Mitchell\Desktop\DotaScraper\drive-download-20230608T152213Z-001\new_venv\Scripts\python.exe DotaScraper.py
